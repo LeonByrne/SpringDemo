1a68be62e65313ef0154045cf63cb26691d8aacc
LeonByrne <leonbyrne52@gmail.com>
2024-01-29 23:29:37 +0000

    First examples.

---
+++ src/main/java/BackendTeam/demo/DemoController.java
@@ -0,0 +1,84 @@
+package BackendTeam.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class DemoController
+{
+    /*
+
+     */
+
+    @GetMapping("/hello")  // The mapping tells spring where the function is called from. Eg http://localhost:8080/hello
+    public String hello()    // The name can be whatever you want but the type and being public matters.
+    {
+        return "Hi.";
+    }
+
+    // There are two ways to pass variables to the server, one is through path variables
+    @GetMapping("/I_am_{name}")  // Whatever is in place of {name} will be given as a variable.
+    public String name(@PathVariable String name)   // We need to specify the type.
+    {
+        return "Nice to meet you " + name;  // We can use the string passed through the path in the function.
+    }
+
+    @GetMapping("/age_is_{age}")
+    public String age(@PathVariable int age)  // Path variables can be integers, floats, etc. Spring will convert it for you
+    {
+        return "You're " + age + " years old.";
+    }
+
+
+    /*
+        Path variables can only do so much. Request bodies can send much more complicated data types.
+
+        Request bodies allow us to send whole objects, even ones we create ourselves
+
+        Don't worry about how data is sent to this, but here is an example command
+
+        curl -d '{"name":"Leon Byrne", "age": 20}' -H "Content-Type: application/json" -X POST http://localhost:8080/person
+    */
+    @PostMapping("/person")
+    public String person(@RequestBody PersonDTO person)
+    {
+        return "Your name is " + person.getName() + " and you're " + person.getAge() + " years old.";
+    }
+
+    /*
+        Status codes can be returned to indicate the status of the server, whether the data sent is done so correctly,
+        whether the user is allowed or meant to access the resource, etc.
+
+        curl -d '2' -H 'Content-Type: application/json' -X POST http://localhost:8080/only_twos
+     */
+    @PostMapping("/only_twos")
+    public HttpStatus twoOnly(@RequestBody Integer num)
+    {
+        if(num == 2)
+        {
+            return HttpStatus.OK;
+        } else
+        {
+            return HttpStatus.NOT_ACCEPTABLE;
+        }
+    }
+
+    /*
+        We can send data back in addition to a status.
+
+        We just have to wrap them in a response entity.
+     */
+    @PostMapping("/sqrt")
+    public ResponseEntity<Double> sqrt(@RequestBody Double num)
+    {
+        if(num < 0.0f)
+        {
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        } else
+        {
+            return new ResponseEntity<>(Math.sqrt(num), HttpStatus.OK);
+        }
+    }
+
+}
